0435312dcf196e6cd0f1082dbc8b9d62c3808198
qamarkhanInnovo <wqasimg@gmail.com>
2024-10-30 15:28:59 +0500

    whatsapp icon is added

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -10,6 +10,7 @@ import {
 } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import useSettings from "../hooks/useSettings";
 import { topBarHeight } from "../utils/constant";
@@ -66,12 +67,22 @@ const Footer = () => {
               >
                 <GitHubIcon sx={{ mr: 1, fontSize: "2em" }} />
               </Link>
+
               <Link
                 href={"https://www.facebook.com/salman.naike"}
                 target="_blank"
                 sx={{ color: "white" }}
               >
                 <FacebookIcon sx={{ mr: 1, fontSize: "2em" }} />
+              </Link>
+
+              <Link
+                href={`https://wa.me/923244929494?text=${encodeURIComponent("Hello Waseem")}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                sx={{ color: "white" }}
+              >
+                <WhatsAppIcon sx={{ mr: 1, fontSize: "2em" }} />
               </Link>
             </Span>
 
